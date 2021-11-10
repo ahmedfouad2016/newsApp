d9077584cedf1fb768b4a2bbbc5e2abb4f148381
Ahmed <ahmed.fouad@gmail.com>
2021-11-10 05:46:15 +0300

    feat: add react native config

--- App/Config/Constants/index.ts
+++ App/Config/Constants/index.ts
@@ -1,6 +1,10 @@
+import Config from 'react-native-config';
+
 const Constant = {
-  BASE_URL: '',
+  BASE_URL: Config.API_URL,
+  API_KEY: Config.API_KEY,
   REQUEST_TIME_OUT: 30000,
+  PAGE_SIZE: 10,
 };
 
 export default Constant;
